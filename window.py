7bb546f5404b175d8aabf2ec1533ec7ce8e47549
l-hartman <ljhartman10@gmail.com>
2020-05-06 16:52:34 -0700

    line drawing

--- window.py
+++ window.py
@@ -14,8 +14,10 @@ class Window(object):
         self.w = w
         self.h = h
         ti.init(arch=arch)
+        self.line_pixels = self.line_coordinates(10, 10, 100, 100)
         self.pixels = ti.var(dt=ti.f32, shape=(w, h))
         self.gui = ti.GUI(self.title, (self.w, self.h))
+        
 
     def draw(self, frames):
         for i in range(frames):
@@ -26,12 +28,19 @@ class Window(object):
     @ti.kernel
     def paint(self, t: ti.f32):
         for i, j in self.pixels: # Parallized over all pixels
-            if i < 50: 
-                self.pixels[i, j] = white
+            if self.in_tuple_list(i, j):
+                self.pixels[i, j] = 0
             else:
-                self.pixels[i, j] = black
-            
+                self.pixels[i, j] = 1
     
+    def in_tuple_list(self, x, y):
+                
+        if x >= self.line_pixels[0][0] and x <= self.line_pixels[len(self.line_pixels) - 1][0]:
+                if y >= self.line_pixels[0][1] and y <= self.line_pixels[len(self.line_pixels) - 1][1]:
+                    return True
+        return False
+
+
     def line_coordinates(self, x1, y1, x2, y2):
         """generates line coordinates from starting position to end
 
@@ -44,27 +53,19 @@ class Window(object):
         returns:
             list of x and y coordinates representing line
         """
-        m = (y2 - y1) /  (x2 - x1)
+        m = (y2 - y1) / (x2 - x1)
         err = 0
         y_curr = y1
 
-        pixels =[()]
+        line_pixels = []
 
         for x in range(x1, x2+1):
-            pixels.append((x, y_curr))
+            line_pixels.append((x, y_curr))
             err += m
-            if abs(m) < 0:
-                if err < -0.5:
-                    y_curr -= 1
-                    err += 1
-            else:
-                if err > 0.5:
-                    y_curr += 1
-                    err -= 1
-        
-
-        
-
+            if err > 0.5:
+                y_curr += 1
+                err -= 1
+            
 """
     def axis(self, x_axis, y_axis, x_range, y_range):
         self.x_axis = x_axis
@@ -79,4 +80,3 @@ def complex_sqr(z):
 
 w = Window()
 w.draw(100)
-print(w.line_coordinates(1,1,10,10))
